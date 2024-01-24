79a6d2a4f50016c1a2181040fd26c96ec39fc76d
Mariano-JR <mariano_br123@hotmail.com>
2024-01-24 19:28:18 -0300

    🛠 Fix: Includes register routes if exist

--- src/main/services/RegisterClientService.ts
+++ src/main/services/RegisterClientService.ts
@@ -1,8 +1,12 @@
 import { Client } from "../../models/client";
 import { clientsRepository } from "../../infra/database/repositories/client";
+import { AppError } from "../errors/AppError";
 
 class RegisterClientService {
     execute(client: Client) {
+        const clientExist = clientsRepository.list().find(values => values.cpf === client.cpf)
+        if(clientExist) throw new AppError("CPF já cadastrado")
+        
         const newClient = clientsRepository.save(client)
         return newClient
     }
--- src/main/services/RegisterVehicleService.ts
+++ src/main/services/RegisterVehicleService.ts
@@ -1,8 +1,12 @@
-import { vehiclesRepository } from "../../infra/database/repositories/vehicle";
+import { AppError } from "../errors/AppError";
 import { Vehicle } from "../../models/vehicle";
+import { vehiclesRepository } from "../../infra/database/repositories/vehicle";
 
 class RegisterVehicleService {
     execute(vehicle: Vehicle) {
+        const vehicleExist = vehiclesRepository.findByPlate(vehicle.plate)
+        if(vehicleExist) throw new AppError("Placa já cadastrado")
+
         const newVehicle = vehiclesRepository.save(vehicle)
         return newVehicle
     }
