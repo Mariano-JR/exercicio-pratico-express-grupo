f796cfb72b602471d2cd116f4aa0a32b11b5b435
Enei <eneipereira@outlook.com.br>
2024-01-23 23:22:48 -0300

    feat: Adiciona um middleware de validação de entrada de dados para novos clientes e veiculos

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -3,6 +3,8 @@ import { clientsController } from './controllers/client'
 import { vehiclesController } from './controllers/vehicle'
 import { rentsController } from './controllers/rent'
 import { errorHandlerMiddleware } from './middlewares/ErrorHandlerMiddleware'
+import { vehicleMiddleware } from './middlewares/VehicleMiddleware'
+import { clientMiddleware } from './middlewares/ClientMiddleware'
 
 
 export const routes = Router()
@@ -13,8 +15,8 @@ routes.get('/vehicles/:license_type/rented', vehiclesController.listHTTPRented)
 routes.get('/vehicles/:license_type/available', vehiclesController.listHTTPAvailable)
 
 routes.post('/rents', rentsController.registerHTTP)
-routes.post('/clients', clientsController.registerHTTP)
-routes.post('/vehicles', vehiclesController.registerHTTP)
+routes.post('/clients', clientMiddleware.validateNewClient, clientsController.registerHTTP)
+routes.post('/vehicles', vehicleMiddleware.validateNewVehicle, vehiclesController.registerHTTP)
 
 routes.put('/rent/:id', rentsController.returnHTTP)
 
