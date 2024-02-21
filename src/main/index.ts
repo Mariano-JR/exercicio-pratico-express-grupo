2b0398de347970f73104b700ea2bdbc68b8bf367
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-21 20:49:39 -0300

    refactor: ajustado código do rateLimitMiddleware

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,29 +1,19 @@
+import { rateLimitMiddleware } from '../common/middlewares/rate-limit';
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express'
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import expressRateLimit from 'express-rate-limit';
 import { routes } from './routes'
 
+dotenv.config();
 export const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
-
-const rateLimitMs = Number(process.env.RATE_LIMIT_MS)
-const rateLimitMin = rateLimitMs / 60000
-const rateLimitMax = Number(process.env.RATE_LIMIT_MAX)
-
-app.use(expressRateLimit({
-  windowMs: rateLimitMs,
-  max: rateLimitMax,
-  message: `Você excedeu o limite de ${rateLimitMax} requisições por minuto, volte novamente em ${rateLimitMin} ${rateLimitMin == 1 ? 'minuto' : 'minutos'}!`
-}));
+app.use(rateLimitMiddleware());
 
 app.use(routes);
 
