6812891ddd0c39bea84a3d9852f0fd91bfdf94c6
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-21 19:35:23 -0300

    feat: adicionado rate limit

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,7 @@ import express from 'express'
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import expressRateLimit from 'express-rate-limit';
 
 import { routes } from './routes'
 
@@ -13,7 +14,14 @@ export const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+
+app.use(expressRateLimit({
+  windowMs: Number(process.env.RATE_LIMIT_MS),
+  max: Number(process.env.RATE_LIMIT_MAX),
+  message: `Você excedeu o limite de requisições de ${process.env.RATE_LIMIT_MAX} por minuto, volte novamente em ${Number(process.env.RATE_LIMIT_MS) / 60000} minutos!`
+}));
+
+app.use(routes);
 
 app.listen(process.env.PORT, () => console.log(`Servidor rodando em http://localhost:${process.env.PORT}`))
