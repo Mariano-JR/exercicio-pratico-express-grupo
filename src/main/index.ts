9672e1edaab03fbd37e708e1b394985ddaf250e3
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-21 19:59:20 -0300

    refactor: ajustado código

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,7 +6,6 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import expressRateLimit from 'express-rate-limit';
-
 import { routes } from './routes'
 
 export const app = express();
@@ -16,10 +15,14 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
+const rateLimitMs = Number(process.env.RATE_LIMIT_MS)
+const rateLimitMin = rateLimitMs / 60000
+const rateLimitMax = Number(process.env.RATE_LIMIT_MAX)
+
 app.use(expressRateLimit({
-  windowMs: Number(process.env.RATE_LIMIT_MS),
-  max: Number(process.env.RATE_LIMIT_MAX),
-  message: `Você excedeu o limite de requisições de ${process.env.RATE_LIMIT_MAX} por minuto, volte novamente em ${Number(process.env.RATE_LIMIT_MS) / 60000} minutos!`
+  windowMs: rateLimitMs,
+  max: rateLimitMax,
+  message: `Você excedeu o limite de ${rateLimitMax} requisições por minuto, volte novamente em ${rateLimitMin} ${rateLimitMin == 1 ? 'minuto' : 'minutos'}!`
 }));
 
 app.use(routes);
