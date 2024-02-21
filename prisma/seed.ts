12f6ac2e6a30bb857c50c9d6b9f084a20eb249e7
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-21 19:08:28 -0300

    feat: adicionado novos registros na seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,37 +4,76 @@ import { LicenseTypeEnum, RentalStatusEnum, VehicleTypeEnum } from '../src/commo
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.client.create({
-    data: {
-      id: '52e52322-244f-4050-90f7-92e242419cb1',
-      cpf: '12345678901',
-      name: 'João da Silva',
-      license_type: LicenseTypeEnum.A,
-    },
+  await prisma.client.createMany({
+    data: [
+      {
+        id: '52e52322-244f-4050-90f7-92e242419cb1',
+        cpf: '12345678901',
+        name: 'João da Silva',
+        license_type: LicenseTypeEnum.A,
+      },
+      {
+        id: '52e52322-244f-4050-90f7-92e242419cb2',
+        cpf: '12345678902',
+        name: 'Maria da Silva',
+        license_type: LicenseTypeEnum.B,
+      }
+    ],
   })
 
-  await prisma.vehicle.create({
-    data: {
-      id: '028baac4-46ae-11ee-be56-0242ac120002',
-      plate: 'ABC1234',
-      type: VehicleTypeEnum.CAR,
-      model: 'Ford Ka',
-      daily_value: 100.00,
-      license_type: LicenseTypeEnum.B,
-      available: false,
-    },
+  await prisma.vehicle.createMany({
+    data: [
+      {
+        id: '028baac4-46ae-11ee-be56-0242ac120001',
+        plate: 'DEF5678',
+        type: VehicleTypeEnum.MOTORCYCLE,
+        model: 'CB500',
+        daily_value: 80.00,
+        license_type: LicenseTypeEnum.A,
+        available: false,
+      },
+      {
+        id: '028baac4-46ae-11ee-be56-0242ac120002',
+        plate: 'ABC1234',
+        type: VehicleTypeEnum.CAR,
+        model: 'Ka 1.5',
+        daily_value: 120.00,
+        license_type: LicenseTypeEnum.B,
+        available: false,
+      },
+      {
+        id: '028baac4-46ae-11ee-be56-0242ac120003',
+        plate: 'EKC7567',
+        type: VehicleTypeEnum.MOTORCYCLE,
+        model: 'Fan 125',
+        daily_value: 30.00,
+        license_type: LicenseTypeEnum.B,
+        available: true,
+      }
+    ],
   })
 
-  await prisma.rental.create({
-    data: {
-      id: '09fb0336-46ae-11ee-be56-0242ac120002',
-      start_date: new Date(),
-      daily_value: 100.00,
-      amount: 0,
-      client_id: '52e52322-244f-4050-90f7-92e242419cb1',
-      vehicle_id: '028baac4-46ae-11ee-be56-0242ac120002',
-      status: RentalStatusEnum.PROGRESS,
-    },
+  await prisma.rental.createMany({
+    data: [
+      {
+        id: 'bdd24359-63a8-44ce-ad23-8e50bbff9f61',
+        start_date: new Date(),
+        daily_value: 50.00,
+        amount: 0,
+        client_id: '52e52322-244f-4050-90f7-92e242419cb1',
+        vehicle_id: '028baac4-46ae-11ee-be56-0242ac120001',
+        status: RentalStatusEnum.PROGRESS,
+      },
+      {
+        id: 'bdd24359-63a8-44ce-ad23-8e50bbff9f62',
+        start_date: new Date(),
+        daily_value: 100.00,
+        amount: 0,
+        client_id: '52e52322-244f-4050-90f7-92e242419cb2',
+        vehicle_id: '028baac4-46ae-11ee-be56-0242ac120002',
+        status: RentalStatusEnum.PROGRESS,
+      }
+    ],
   })
 }
 
