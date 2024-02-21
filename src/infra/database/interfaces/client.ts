fd6fa799ea0cbd41e957c6360a493fb1999c4628
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-21 18:30:14 -0300

    refactor: removido arquivos desnecessários

--- src/infra/database/interfaces/client.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from "../../../models/client"
-
-export interface ClientRepositoryInterface {
-    list(): Client[]
-    findByCpf(cpf: string): Client | undefined
-    save(client: Client): Client
-}
--- src/infra/database/interfaces/rent.ts
+++
@@ -1,9 +0,0 @@
-import { Rent } from "../../../models/rent"
-
-export interface RentRepositoryInterface {
-    findById(id: number): Rent | undefined
-    findByClientId(client_id: number): Rent[] | undefined
-    findByClientIdAndStatus(client_id: number, status: string): Rent | undefined
-    save(rent: Rent): Rent
-    updateReturn(rent: Rent): Rent
-}
